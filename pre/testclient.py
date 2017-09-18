d8685d30077e5301f81b00b4520d1314d5faa690
Dag Brattli <dag@brattli.net>
2017-09-18 08:38:02 +0200

    Make client more async
    Will now receive messages from server while running

--- pre/testclient.py
+++ pre/testclient.py
@@ -23,7 +23,7 @@ class WSClient:
 
     def on_message(self, msg):
         global running
-        print("on_message", msg)
+        #print("on_message", msg)
         if msg is None:
             running = False
 
@@ -38,6 +38,11 @@ class WSClient:
         self.conn = yield websocket_connect(url, on_message_callback=self.on_message)
 
 
+    def close(self):
+        self.conn.close()
+
+
+@gen.coroutine
 def main():
     global running
     start = None
@@ -47,11 +52,13 @@ def main():
 
         ws.write({"lat": point.lat, "lng": point.lng, "time": point.time.isoformat(), "alt": point.alt, "user": point.user})
 
+    wss = []
+
     print("Parsing GPS files")
     for filename in glob.glob(os.path.join("data", "*.gpx")):
         username = filename.split(os.sep)[1].split(".")[0]
         ws = WSClient(username)
-        ioloop.IOLoop.current().run_sync(ws.connect)
+        wss.append(ws)  # Connect later
 
         points = Point.from_gpxfile(filename)
 
@@ -63,15 +70,21 @@ def main():
             scheduler.schedule_absolute(point.time, action, state=(ws, point))
 
     print("Starting at: ", start.time, start)
-    scheduler.advance_to(start.time)
 
+    # Connect all clients after server set to virtual time
+    for ws in wss:
+        yield ws.connect()
+
+    scheduler.advance_to(start.time)
     while running:
-        time.sleep(0.1)
+        yield gen.sleep(0.1)
         scheduler.advance_by(timedelta(seconds=20))
+
+        ws.write({"time": scheduler.now.isoformat()})
 
         if not scheduler.queue.peek():
             break
 
 
 if __name__ == '__main__':
-    main()
+    ioloop.IOLoop.current().run_sync(main)
